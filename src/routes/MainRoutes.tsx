261bb5ae9544c1b4b271e77ce052308818576446
Sebastian <sebastian.hernandorv@gmail.com>
2025-03-30 14:16:48 -0500

    update

--- src/routes/MainRoutes.tsx
+++ src/routes/MainRoutes.tsx
@@ -1,14 +1,9 @@
 import { RouteObject } from "react-router-dom";
 import App from "../App";
-import { Foro } from "../components";
 
 export const MainRoutes: RouteObject[] = [
   {
     path: "/",
     element: <App />,
-  },
-  {
-    path: "/foro",
-    element: <Foro />,
-  },
+  }
 ];
